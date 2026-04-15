81b8984cd69c8137808d14f861648d8e9ef862d1
WilfridoCastillo1969 <wilfridodavid69@gmail.com>
2026-04-15 10:35:39 -0600

    Add files via upload

---
+++ pareimpar.py
@@ -0,0 +1,7 @@
+# Hacer un programa que lea un número entero e imprima
+# es par o impar
+numero = int(input("Ingrese un número entero "))
+if numero % 2 ==0:
+    print("El número que ingrese es par")
+else:
+    print("El número que ingrese es impar")
